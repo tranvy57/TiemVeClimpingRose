abc1c4d15836ede4469aadc7bb619b78236ad633
zy <tranvy.art@gmail.com>
2025-06-15 01:50:42 +0700

    update: replace BaseResponse with ApiResponse in login API

--- FE/tiem-ve-climping-rose-fe/src/api/auth/login.ts
+++ FE/tiem-ve-climping-rose-fe/src/api/auth/login.ts
@@ -1,4 +1,4 @@
-import { BaseResponse } from "@/types";
+import { ApiResponse } from "@/types";
 import { IUser } from "@/types/implements";
 import axios from "axios";
 
@@ -10,7 +10,7 @@ export interface LoginRequest {
 export const login = async (body: LoginRequest) => {
   try {
     const response = await axios.post<
-      BaseResponse<{ token: string; user: IUser }>
+      ApiResponse<{ token: string; user: IUser }>
     >(
       `${process.env.NEXT_PUBLIC_API_URL}${process.env.NEXT_PUBLIC_API_URL_USER_SERVICE}/auth/token`,
       body
