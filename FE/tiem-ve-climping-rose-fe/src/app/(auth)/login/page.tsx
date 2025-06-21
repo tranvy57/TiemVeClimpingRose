e2eb69c1cfb59dc2fe36a39ec69ff84b1fff58ec
zy <tranvy.art@gmail.com>
2025-06-21 22:47:22 +0700

    fix: correct SVG attributes

--- FE/tiem-ve-climping-rose-fe/src/app/(auth)/login/page.tsx
+++ FE/tiem-ve-climping-rose-fe/src/app/(auth)/login/page.tsx
@@ -70,8 +70,8 @@ export default function LoginForm({
                 <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                 <g
                   id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></g>
                 <g id="SVGRepo_iconCarrier">
                   <path
