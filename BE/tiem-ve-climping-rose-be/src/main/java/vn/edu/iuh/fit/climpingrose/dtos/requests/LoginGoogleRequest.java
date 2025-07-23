6fd4c86706e93216c0f6dbf6a9c5780781d69187
zy <tranvy.art@gmail.com>
2025-07-23 15:55:46 +0700

    add: login goole

---
+++ BE/tiem-ve-climping-rose-be/src/main/java/vn/edu/iuh/fit/climpingrose/dtos/requests/LoginGoogleRequest.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.fit.climpingrose.dtos.requests;
+
+public class LoginGoogleRequest {
+}
---
+++ BE/tiem-ve-climping-rose-be/src/main/java/vn/edu/iuh/fit/climpingrose/enums/AuthProvider.java
@@ -0,0 +1,4 @@
+package vn.edu.iuh.fit.climpingrose.enums;
+
+public class AuthProvider {
+}
